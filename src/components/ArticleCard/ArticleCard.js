e94187005080d6f51d94d2acb883e1a381f60eee
jordan smith <jordan.smith6035@gmail.com>
2023-04-11 13:30:46 -0600

    Add article cards

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,22 +1,31 @@
-import './ArticleCard.css'
-import { Link } from 'react-router-dom'
-
+import './ArticleCard.css';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function ArticleCard({ id, title, media, section, url }) {
-    return (
-        <div>
-            <h2>{title}</h2>
-            <img 
-            src={media}
-            alt={`${title} image`}
-            />
-            <p>{section}</p>
-            <a href={url} target="_blank" rel='noopener noreferrer'>
-                <button>Go to Article</button>
-            </a>
-            <Link to={`/article/details/${id}`}>
-                <button>Learn More</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="article-card">
+      <h2>{title}</h2>
+      {media && <img src={media} alt={`${title} image`} />}
+      <p>{section}</p>
+      <div className="article-buttons">
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <button>Go to Article</button>
+        </a>
+        <Link to={`/article/details/${id}`}>
+          <button>Learn More</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
+
+ArticleCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  media: PropTypes.string,
+  section: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
+};
+
+export default ArticleCard;
